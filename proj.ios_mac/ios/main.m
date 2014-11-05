3234360f44fcae4515c898b3d55b1783f704c2f0
Luckyhu <huji0624@gmail.com>
2014-11-05 23:09:58 +0800

    fix issue for xcode6.
    fyi.

--- proj.ios_mac/ios/main.m
+++ proj.ios_mac/ios/main.m
@@ -7,3 +7,19 @@ int main(int argc, char *argv[]) {
     [pool release];
     return retVal;
 }
+
+#include <stdio.h>
+//FILE *fopen$UNIX2003( const char *filename, const char *mode )
+//{
+//    return fopen(filename, mode);
+//}
+size_t fwrite$UNIX2003( const void *a, size_t b, size_t c, FILE *d )
+{
+    return fwrite(a, b, c, d);
+}
+int strerror$UNIX2003(int i){
+    return strerror(i);
+}
+
+
+
